530c9ab3bc2774077e44faf1df2bbf4fcaf28db0
SphereCat1 <spherecat1@gmail.com>
2011-12-04 14:38:39 -0500

    Initial Commit

---
+++ ujd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ujd
+//
+//  Created by Billy Doyle on 12/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
